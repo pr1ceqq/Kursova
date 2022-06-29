d8bbbf1f058178f6cc105323a1c2cd3a40456241
pr1ceqq <90266115+pr1ceqq@users.noreply.github.com>
2022-06-29 14:31:53 +0300

    Update Const.cs

--- Const.cs
+++ Const.cs
@@ -3,6 +3,6 @@
     public class Const
     {
         public static string adress = "https://kitsu.io/api/edge/";
-        public static string telegramBotToken = "5513421079:AAFJHPVkZWYzFXza636DJYyfDRdB9IlJ1JY";
+        public static string telegramBotToken = "";
     }
 }
